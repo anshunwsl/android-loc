a62344db54b14e46ef2a6d3bcf0fd0792f956bb2
agisv2 <agisv2@sina.cn>
2019-06-18 19:41:07 +0800

    init project

---
+++ app/src/main/java/com/anshunwsl/androidloc/demo/QuestActivity.java
@@ -0,0 +1,20 @@
+package com.anshunwsl.androidloc.demo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.anshunwsl.androidloc.R;
+
+//import com.baidu.baidulocationdemo.R;
+
+/**
+ * 常见问题
+ */
+public class QuestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quest_activity);
+    }
+}
